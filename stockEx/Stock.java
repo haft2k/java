7747241918953323aa456cb63910c786a76865b6
falc0n <haft2k@gmail.com>
2022-07-22 17:45:58 +0700

    a
    a

--- stockEx/Stock.java
+++ stockEx/Stock.java
@@ -1,9 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package stockEx;
 
+/**
+ *
+ * @author Admin
+ */
 public class Stock {
-	private String sIDStock;
-
-	Stock(String sId) {
-		sIDStock = sId;
-	}
+    private String sIDStock;
+    Stock(String sId) {
+        sIDStock=sId;
+        ///
+    }
 }
